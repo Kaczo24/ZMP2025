20b77099feb63780034d6c8ec0652ccf1449f4f8
Kaczo24 <55463087+Kaczo24@users.noreply.github.com>
2025-03-10 11:37:23 +0100

    zad1

---
+++ l1/zi/287358_z1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int f(int x) {
+    if(x&1==1) return 3*x + 1;
+    return x >> 1;
+}
+
+int main() {
+    int liczba;
+    cout << "Podaj liczbę:";
+    cin >> liczba;
+    
+    int count = 1;
+    cout << liczba;
+    while(liczba != 1) {
+        liczba = f(liczba);
+        cout << " " << liczba;
+        count++;
+    }
+    cout << ", " << count;
+    
+    return 0;
+}
