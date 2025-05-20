2248439befa3550bd2732bb3fc6725b019bda7c8
Kaczo24 <55463087+Kaczo24@users.noreply.github.com>
2025-05-20 23:44:38 +0200

    l8 zad 2 part 2

--- l8/z2/287358_z2.cpp
+++ l8/z2/287358_z2.cpp
@@ -2,104 +2,116 @@
 
 using namespace std;
 
-struct Node {
-    int key;
-    Node* left;
-    Node* right;
-      
-    Node(){
-        key=0;
-        left=nullptr;
-        right=nullptr;
-    }
-    Node(int val){
-        key=val;
-        left=nullptr;
-        right=nullptr;
-    }
-    ~Node(){
-        delete left;
-        delete right;
-    }
-    void show_node(){
-        if (left!=nullptr)
-            (*left).show_node();
-        cout << key << " ";
-        if (right!=nullptr)
-            (*right).show_node();
+class Btree {
+private:
+    struct Node {
+        int key = 0;
+        Node* left = nullptr;
+        Node* right = nullptr;
+          
+        Node(){}
+        Node(int val){
+            key = val;
+        }
+        Node(int val, Node* _left, Node* _right){
+            key = val;
+            left = _left;
+            right = _right;
+        }
+        ~Node(){
+            delete left;
+            delete right;
+        }
+        
+        void show_node(){
+            if (left!=nullptr)
+                (*left).show_node();
+            cout << key << " ";
+            if (right!=nullptr)
+                (*right).show_node();
+        }
+        
+        bool operator==(Node &other) {
+            if(this == nullptr && &other == nullptr)  return true;
+            if(this == nullptr || &other == nullptr)  return false;
+            return (*left == *other.left) && (*right == *other.right);
+        }
+        
+        Node operator+(Node &other) {
+            if(this == nullptr || &other == nullptr)  return *this;
+            
+            Node a, b, result = Node(key + other.key);
+            if(left != nullptr) {
+                a = (*left) + (*other.left);
+                result.left = &a;
+            }
+            if(right != nullptr) {
+                a = (*right) + (*other.right);
+                result.right = &a;
+            }
+            return Node(key + other.key, &a, &b);
+        }
+    };
+    
+    Node* root = nullptr;
+    
+public:
+    
+    Btree(){}
+    ~Btree(){
+        delete root;
     }
     
-    bool operator==(Node* other) {
-        if(this == nullptr && other == nullptr)  return true;
-        if(this == nullptr && other != nullptr)  return false;
-        if(this != nullptr && other == nullptr)  return false;
-        return (this->left == other->left) && (this->right == other->right);
-    }
-  
-};
-
-class Btree {
-  private:
-    Node* root;
-  public:
-    Btree(){
-      root=nullptr;
-    }
-    ~Btree(){
-      delete root;
-    }
     void insert(int new_key){
-      insert_node_aux(root,new_key);
+        insert_node_aux(root,new_key);
     }
-    void insert_node_aux(Node*& node, int new_key){
-      if (node == nullptr){
-        node=new Node(new_key);
-      }
-      else if (new_key < node->key){
-        insert_node_aux(node->left,new_key);
-      }
-      else {
-        insert_node_aux(node->right,new_key);
-      }
+    
+    void insert_node_aux(Node*& node, int new_key) {
+        if (node == nullptr) 
+            node=new Node(new_key);
+        else if (new_key < node->key)
+            insert_node_aux(node->left,new_key);
+        else 
+            insert_node_aux(node->right,new_key);
     }
-    void show_tree(){
+    
+    void show_tree() {
       root->show_node();
       cout << "\n";
     }
     
-    bool operator==(Btree* other) {
-        if(this->root == nullptr && other->root == nullptr) return true;
-        return *(this->root)==(other->root);
+    bool operator==(Btree &other) {
+        if(this->root == nullptr && other.root == nullptr) return true;
+        return *(this->root)==*(other.root);
+    }
+    bool operator!=(Btree other) {return !(*this == other);}
+    
+    Btree operator+(Btree &other) {
+        if(*this != other) return *this;
+        Btree result = Btree();
+        *result.root = (*this->root + *other.root);
+        return result;
     }
     
 };
 
 int main(){
-  Btree tree;
+  Btree tree = Btree();
   tree.insert(10);
   tree.insert(8);
-  tree.insert(15);
-  tree.insert(7);
-  tree.insert(9);
-  tree.insert(12);
   tree.insert(19);
   tree.show_tree();
   
-  Btree tree2;
+  Btree tree2 = Btree();
   tree2.insert(10);
   tree2.insert(8);
-  tree2.insert(15);
-  tree2.insert(7);
-  tree2.insert(9);
-  tree2.insert(12);
-  
-  
-  cout << (tree == &tree2);
-  
   tree2.insert(19);
+  tree2.show_tree();
   
-  cout << (tree == &tree2);
   
+  
+  auto res = (tree + tree2);
+  res.show_tree();
   
   return 0;
 }
